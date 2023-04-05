7cb6d3e5b08654a4c422ecbd7399ccb3eb6a706f
Andrew Nash <anash@mun.ca>
2023-04-05 14:16:41 -0230

    v0 urdf publishing robot model

--- boat2023_nav2/launch/carla_nav.launch.py
+++ boat2023_nav2/launch/carla_nav.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -8,10 +9,17 @@ import os
 def generate_launch_description():
 
     pkg_share = get_package_share_directory('boat2023_nav2')
+    model_path = os.path.join(pkg_share, 'src', 'description', 'boat2023.urdf')
     costmap_params_file = os.path.join(pkg_share, 'params', 'costmap_params.yaml')
     ekf_params_file = os.path.join(pkg_share, 'params', 'ekf_params.yaml')
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'gui',
+            default_value='True',
+            description='Flag to enable joint_state_publisher_gui'
+        ),
+
         DeclareLaunchArgument(
             'costmap_params',
             default_value=costmap_params_file,
@@ -22,6 +30,12 @@ def generate_launch_description():
             'ekf_params',
             default_value=ekf_params_file,
             description='Path to the EKF parameters file.'
+        ),
+
+        DeclareLaunchArgument(
+            'model', 
+            default_value=model_path,
+            description='Path to robot urdf file'
         ),
 
         Node(
@@ -44,12 +58,36 @@ def generate_launch_description():
         ),
 
         Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        ),
+
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            condition=UnlessCondition(LaunchConfiguration('gui'))
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            condition=IfCondition(LaunchConfiguration('gui'))
+        ),
+
+        Node(
             package='nav2_costmap_2d',
             executable='nav2_costmap_2d',
             name='nav2_costmap_2d',
             output='screen',
             namespace='/costmap',
-            parameters=[LaunchConfiguration('costmap_params')],
+            parameters=[
+                costmap_params_file,
+                {'global_frame': 'odom'},
+                {'robot_base_frame': 'ego_vehicle'}
+            ],
         ),
         
         Node(
