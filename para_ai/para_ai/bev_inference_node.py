47f4f6584b51f2bc95b70510a66d0998cf0c0899
Andrew Nash <anash@mun.ca>
2023-05-25 16:47:44 -0230

    optimise inference speed

--- para_ai/para_ai/bev_inference_node.py
+++ para_ai/para_ai/bev_inference_node.py
@@ -1,4 +1,3 @@
-import cv2
 import rclpy
 import torch
 import numpy as np
@@ -13,8 +12,10 @@ from cross_view_transformer.visualizations.common import BaseViz
 
 
 class BEVInferenceNode(Node):
-    batch = None
     camera_names = ['f', 'fl', 'fr', 'b', 'bl', 'br']
+    camera_name_to_idx = {name: idx for idx, name in enumerate(camera_names)}
+
+    batch = None
     transform = transforms.Compose([transforms.ToTensor()])
 
     def __init__(self):
@@ -24,13 +25,13 @@ class BEVInferenceNode(Node):
         self.calibration_file = self.declare_parameter('calibration_file').value
         self.threshold = self.declare_parameter('threshold').value
 
-        self.target_fps = 30  # Set your desired FPS
+        self.target_fps = 30  # Set desired FPS
         self.timer_period = 1.0 / self.target_fps
         self.timer = self.create_timer(self.timer_period, self.inference)
 
         self.init_network()
         self.init_bridge()
-        self.init_calibration()
+        self.init_batch()
         self.init_subscribers()
         self.init_publisher()
 
@@ -40,16 +41,23 @@ class BEVInferenceNode(Node):
         self.network.to(self.device)
         self.network.eval()
         self.get_logger().info(f'Using device: {self.device}')
-
-        self.viz = BaseViz()
+        # self.viz = BaseViz()
 
     def init_bridge(self):
         self.bridge = CvBridge()
 
-    def init_calibration(self):
+    def init_batch(self):
         with np.load(self.calibration_file, allow_pickle=True) as data:
             self.extrinsics = torch.from_numpy(data['aux'][()]['extrinsics'].astype('float32')).unsqueeze(0)
             self.intrinsics = torch.from_numpy(data['aux'][()]['intrinsics'].astype('float32')).unsqueeze(0)
+        
+        images = torch.zeros((1, 6, 3, 224, 480), device=self.device)
+
+        self.batch = {
+            'extrinsics': self.extrinsics.to(self.device),
+            'intrinsics': self.intrinsics.to(self.device),
+            'image': images,
+        }
 
     def init_subscribers(self):
         self.image_buffer = {}
@@ -68,43 +76,28 @@ class BEVInferenceNode(Node):
         self.publisher = self.create_publisher(Image, 'para_ai/bev_prediction', 10)
 
     def image_callback(self, camera_name, msg):
-        self.image_buffer[camera_name] = self.bridge.imgmsg_to_cv2(msg)
-        self.prepare_batch()
+        cv_image = self.bridge.imgmsg_to_cv2(msg)
+        tensor_image = self.transform(cv_image).to(self.device)
+        self.batch['image'][0, self.camera_name_to_idx[camera_name]] = tensor_image
 
     def inference(self):
         if self.batch is None:
             return
 
         with torch.no_grad():
-            curr_batch = {k: v.to(self.device) if isinstance(v, torch.Tensor) else v for k, v in self.batch.items()}
-            pred = self.network(curr_batch)
+            pred = self.network(self.batch)
+            pred_bev = pred['bev'][0]
 
             # For debug 6 camera view:
             # curr_batch['bev'] = pred['bev']
             # visualization = np.vstack(self.viz.visualize(batch=curr_batch, pred=pred))
 
-            pred = pred['bev'][0].sigmoid()
-            pred = pred.cpu().numpy().transpose(1, 2, 0)
-            pred = (pred > self.threshold).astype(np.float32)
-            
-            # Convert the array to an image message
-            output_image = self.bridge.cv2_to_imgmsg(pred)
+            # Transfer pred to CPU and apply sigmoid, and thresholding, also convert to numpy
+            occupancy_grid = (torch.sigmoid(pred_bev).cpu().numpy() > self.threshold).astype(np.float32)
+
+            # Transpose and convert the array to an image message
+            output_image = self.bridge.cv2_to_imgmsg(occupancy_grid.transpose(1, 2, 0))
             self.publisher.publish(output_image)
-
-
-    def prepare_batch(self):
-        if len(self.image_buffer) < 6:
-            return
-
-        images = torch.empty(size=(6, 3, 224, 480))
-        for i, camera_name in enumerate(self.camera_names):
-            images[i] = self.transform(self.image_buffer[camera_name])
-
-        self.batch = {
-            'extrinsics': self.extrinsics,
-            'intrinsics': self.intrinsics,
-            'image': images.unsqueeze(0),
-        }
 
 def main(args=None):
     rclpy.init(args=args)
